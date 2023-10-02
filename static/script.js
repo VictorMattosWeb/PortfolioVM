eb6ae521810a7410cd9c23cc86d3069cc924ccf0
Victor Mattos <124466510+VictorMattosWeb@users.noreply.github.com>
2023-10-02 02:37:25 -0300

    Section about and skills

--- static/script.js
+++ static/script.js
@@ -9,3 +9,15 @@ menuMobile.addEventListener('click', () => {
         : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active")
 })
+
+/* Closes the menu when you click on an item and changes the icon to list */
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
